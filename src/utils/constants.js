bf799de52982b0ec2cef943f702502de59d4c8be
Srishti  Jha <72248660+SriCoD@users.noreply.github.com>
2023-12-25 15:25:00 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const LOGO_URL =
-  "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
+   "https://i.pinimg.com/736x/82/be/d4/82bed479344270067e3d2171379949b3.jpg";
 
 export const MENU_API =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
