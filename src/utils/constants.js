94ad42c669b424d2395781b449ebc103e68e59a8
Srishti  Jha <72248660+SriCoD@users.noreply.github.com>
2023-12-27 14:29:35 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,4 @@ export const LOGO_URL =
    "https://i.pinimg.com/736x/82/be/d4/82bed479344270067e3d2171379949b3.jpg";
 
 export const MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
