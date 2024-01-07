ac93c79845782085dafc93f9be18e111d7d12bc5
Srishti  Jha <72248660+SriCoD@users.noreply.github.com>
2024-01-08 01:53:20 +0530

    cors error fixed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,7 @@ export const LOGO_URL =
    "https://i.pinimg.com/736x/82/be/d4/82bed479344270067e3d2171379949b3.jpg";
 
 export const MENU_API =
-  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
+  "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D12.9351929%26lng%3D77.62448069999999%26restaurantId%3D";
+
+  
+  //"https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
